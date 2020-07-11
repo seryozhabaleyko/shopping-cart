04a72b3be80685805b9f443f9d6f894cecdcf9ff
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-11 17:58:59 +0300

    Update categories.js

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -1,22 +1,31 @@
-import * as types from '../constants/types';
+import {
+	GET_CATEGORIES_REQUEST,
+	GET_CATEGORIES_SUCCESS,
+	GET_CATEGORIES_FAILURE
+} from '../constants/actionTypes';
 import { fetchCategories as fetchCategoriesApi } from '../api';
+import { defaultErrorMessage } from '../constants/defaults';
+
+const categoriesRequest = () => ({
+	type: GET_CATEGORIES_REQUEST
+});
+
+const categoriesSuccess = (categories) => ({
+	type: GET_CATEGORIES_SUCCESS,
+	payload: categories
+});
+
+const categoriesFailure = (errorMessage = defaultErrorMessage) => ({
+	type: GET_CATEGORIES_FAILURE,
+	payload: errorMessage
+});
 
 export const fetchCategories = () => async (dispatch) => {
-    dispatch({
-        type: types.categories.START
-    });
-
-    try {
-        const phones = await fetchCategoriesApi();
-        dispatch({
-            type: types.categories.SUCCESS,
-            payload: phones
-        });
-    } catch (err) {
-        dispatch({
-            type: types.categories.FAILURE,
-            payload: err,
-            error: true
-        });
-    }
+	dispatch(categoriesRequest());
+	try {
+		const categories = await fetchCategoriesApi();
+		dispatch(categoriesSuccess(categories));
+	} catch (error) {
+		dispatch(categoriesFailure(error));
+	}
 };
