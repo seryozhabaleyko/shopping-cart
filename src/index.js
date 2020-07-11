dc421384889c00d8d145c4a39d2367e12b58ef7f
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-11 18:00:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import store from './store';
 import App from './App';
 
-const history = createBrowserHistory();
+import './index.scss';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <Router history={history}>
-                <App/>
-            </Router>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root'),
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
