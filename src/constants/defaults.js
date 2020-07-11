4f9faa50e5bdd3ab29484581e9dbb9c771525ec6
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-11 18:00:29 +0300

    Create defaults.js

---
+++ src/constants/defaults.js
@@ -0,0 +1,12 @@
+export const defaultErrorMessage = 'Сервер временно недоступен';
+
+export const manufacturer = [
+    'Xiaomi',
+    'Apple',
+    'Samsung',
+    'Huawei',
+    'Google',
+    'Nokia',
+    'OnePlus',
+    'TeXet',
+];
