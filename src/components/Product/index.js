d69437e667b49fa82b2d8771f492645222f3c268
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-11 18:00:13 +0300

    Create index.js

---
+++ src/components/Product/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { addToCart } from '../../actions/cart';
+
+import styled from './product.module.scss';
+
+function Product({ addToCart, id, name, image }) {
+    return (
+        <div className={styled.product}>
+            <div className={styled.image}>
+                <Link to={`/product/${id}`}>
+                    <img src={image} alt={name} />
+                </Link>
+                <div className={styled.actions}>
+                    <Link to={`/product/${id}`} className={styled.btnLinkView}>
+                        Quick View
+                    </Link>
+                    <button
+                        type="button"
+                        className={styled.btnAddToCart}
+                        onClick={() => addToCart(id)}
+                    >
+                        Add to Cart
+                    </button>
+                </div>
+            </div>
+            <div className={styled.detail}>
+                <h3 className={styled.title}>{name}</h3>
+                <div className={styled.price}>$1500</div>
+            </div>
+        </div>
+    );
+}
+
+const mapDispatchToProps = {
+    addToCart,
+};
+
+export default connect(null, mapDispatchToProps)(Product);
