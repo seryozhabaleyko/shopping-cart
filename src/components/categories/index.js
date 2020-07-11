8c1794657b1b1d60c30bfa9618d256bf1bc6c2f5
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-11 17:59:34 +0300

    Update index.js

--- src/components/categories/index.js
+++ src/components/categories/index.js
@@ -4,24 +4,26 @@ import { NavLink } from 'react-router-dom';
 import useShallowEqualSelector from '../../hooks/useShallowEqualSelector';
 import { getCategories } from '../../selectors';
 
-import './style.scss';
+import styled from './categories.module.scss';
 
-const Categories = () => {
-    const categories = useShallowEqualSelector((state) => getCategories(state));
+function Categories() {
+	const categories = useShallowEqualSelector((state) => getCategories(state));
 
-    return (
-        <section className='categories'>
-            <h4>Brand</h4>
-            <nav>
-                <NavLink to='/' exact>All</NavLink>
-                {categories.map((category, index) => (
-                    <NavLink to={`/categories/${category.id}`} key={index}>
-                        {category.name}
-                    </NavLink>
-                ))}
-            </nav>
-        </section>
-    );
-};
+	return (
+		<section className={styled.categories}>
+			<h4>Brand</h4>
+			<nav>
+				<NavLink to="/" exact>
+					All
+				</NavLink>
+				{categories.map((category, index) => (
+					<NavLink to={`/category/${category.id}`} key={index}>
+						{category.name}
+					</NavLink>
+				))}
+			</nav>
+		</section>
+	);
+}
 
 export default Categories;
