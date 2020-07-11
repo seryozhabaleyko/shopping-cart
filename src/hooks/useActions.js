01be23b9831fb05b699b899e6055eea3ca860acd
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-11 18:00:40 +0300

    Update useActions.js

--- src/hooks/useActions.js
+++ src/hooks/useActions.js
@@ -1,17 +1,18 @@
-import { useMemo } from 'react';
 import { bindActionCreators } from 'redux';
 import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
 
-const useActions = (actions) => {
-    const dispatch = useDispatch();
-
-    return useMemo(() => {
-        if (Array.isArray(actions)) {
-            return actions.map((a) => bindActionCreators(a, dispatch));
-        }
-
-        return bindActionCreators(actions, dispatch);
-    }, [actions, dispatch]);
-};
+function useActions(actions, deps) {
+	const dispatch = useDispatch();
+	return useMemo(
+		() => {
+			if (Array.isArray(actions)) {
+				return actions.map((a) => bindActionCreators(a, dispatch));
+			}
+			return bindActionCreators(actions, dispatch);
+		},
+		deps ? [dispatch, ...deps] : [dispatch]
+	);
+}
 
 export default useActions;
