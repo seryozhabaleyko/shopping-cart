40b998566b0464bf61c3a5e6011f1d2823662cbf
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-13 02:29:59 +0300

    Update filterBy.js

--- src/reducers/filterBy.js
+++ src/reducers/filterBy.js
@@ -1,12 +1,15 @@
-// import * as R from 'ramda';
+import * as R from 'ramda';
+
 import {
     ADD_MANUFACTURER_TO_FILTER,
     REMOVE_MANUFACTURER_FROM_FILTER,
+    SET_MIN_PRICE_FILTER,
+    SET_MAX_PRICE_FILTER,
 } from '../constants/actionTypes';
 
 const initialState = {
     manufacturer: [],
-    price: [],
+    price: { min: 0, max: 1000 },
 };
 
 export default (state = initialState, { type, payload }) => {
@@ -14,13 +17,18 @@ export default (state = initialState, { type, payload }) => {
         case ADD_MANUFACTURER_TO_FILTER:
             return {
                 ...state,
-                manufacturer: state.manufacturer.concat(payload),
+                manufacturer: R.concat(R.prop('manufacturer', state), [payload]),
             };
         case REMOVE_MANUFACTURER_FROM_FILTER:
             return {
                 ...state,
-                manufacturer: state.manufacturer.filter((item) => item !== payload),
+                manufacturer: R.without(payload, R.prop('manufacturer', state)), // state.manufacturer.filter((item) => item !== payload),
             };
+        case SET_MIN_PRICE_FILTER:
+            return { ...state, price: R.assoc('min', payload, R.prop('price', state)) };
+        case SET_MAX_PRICE_FILTER:
+            return { ...state, price: R.assoc('max', payload, R.prop('price', state)) };
+
         default:
             return state;
     }
