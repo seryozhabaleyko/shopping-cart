52fe9639d30995ada479ebd3a18d5f46201d2395
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-15 00:26:37 +0300

    Create RAM.js

---
+++ src/components/Filters/RAM.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import Checkbox from '../Checkbox';
+import { addRAMToFilter, removeRAMFromFilter } from '../../actions/filterBy';
+
+import styled from './RAM.module.scss';
+
+function RAM() {
+    const dispatch = useDispatch();
+
+    const handleCheckboxChange = (event) => {
+        const { checked, name } = event.target;
+        if (checked) {
+            dispatch(addRAMToFilter(name));
+        } else {
+            dispatch(removeRAMFromFilter(name));
+        }
+    };
+
+    return (
+        <div className={styled.ram}>
+            <div className={styled.heading}>RAM</div>
+            {['2', '3', '4', '6', '8', '12'].map((ram) => (
+                <Checkbox
+                    key={ram}
+                    name={ram}
+                    label={`${ram} GB`}
+                    onChange={handleCheckboxChange}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default RAM;
