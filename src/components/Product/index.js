e00372dcafa5f424fc5e3eb1b77011be886ab19b
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-07-15 00:26:43 +0300

    Update index.js

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import classNames from 'classnames';
+import { useDispatch } from 'react-redux';
 
 import { addToCart } from '../../actions/cart';
 
 import styled from './product.module.scss';
 
-function Product({ addToCart, id, name, image, price }) {
+function Product({ id, name, image, price }) {
+    const dispatch = useDispatch();
+
+    const handleAddToCartClick = () => dispatch(addToCart(id));
+
     return (
         <div className={styled.product}>
             <div className={styled.image}>
@@ -17,11 +22,7 @@ function Product({ addToCart, id, name, image, price }) {
                     <Link to={`/product/${id}`} className={styled.btnLinkView}>
                         Quick View
                     </Link>
-                    <button
-                        type="button"
-                        className={styled.btnAddToCart}
-                        onClick={() => addToCart(id)}
-                    >
+                    <button type="button" className={styled.btnAddToCart} onClick={handleAddToCartClick}>
                         Add to Cart
                     </button>
                 </div>
@@ -30,12 +31,15 @@ function Product({ addToCart, id, name, image, price }) {
                 <h3 className={styled.title}>{name}</h3>
                 <div className={styled.price}>{`$ ${price.toLocaleString()}`}</div>
             </div>
+            <button
+                type="button"
+                className={classNames('btn', 'btn-danger', styled.addToCart)}
+                onClick={handleAddToCartClick}
+            >
+                Add to Cart
+            </button>
         </div>
     );
 }
 
-const mapDispatchToProps = {
-    addToCart,
-};
-
-export default connect(null, mapDispatchToProps)(Product);
+export default Product;
