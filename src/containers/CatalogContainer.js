67ab12db0064b2b54591047d18a30e90f066e1a2
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-02 22:10:10 +0300

    Update CatalogContainer.js

--- src/containers/CatalogContainer.js
+++ src/containers/CatalogContainer.js
@@ -1,30 +1,19 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
 import { fetchCatalog } from '../actions/catalog';
 import { fetchCategories } from '../actions/categories';
 import Catalog from '../pages/Catalog';
 
-function CatalogContainer({ fetchCatalog, fetchCategories }) {
+function CatalogContainer() {
+    const dispatch = useDispatch();
+
     useEffect(() => {
-        fetchCatalog();
-        fetchCategories();
+        dispatch(fetchCatalog());
+        dispatch(fetchCategories());
     }, [fetchCatalog, fetchCategories]);
 
-    console.log('CatalogContainer');
-
-    return <Catalog catalog={'catalog'} />;
+    return <Catalog />;
 }
 
-CatalogContainer.propTypes = {
-    fetchCatalog: PropTypes.func.isRequired,
-    fetchCategories: PropTypes.func.isRequired,
-};
-
-const mapDispatchToProps = {
-    fetchCatalog,
-    fetchCategories,
-};
-
-export default connect(null, mapDispatchToProps)(CatalogContainer);
+export default CatalogContainer;
