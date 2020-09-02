d1b7aff9e81f6f7bb39c86d79ea3692aa0f304fe
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-02 22:10:24 +0300

    Create Catalog.js

---
+++ src/pages/Catalog/Catalog.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Layout from '../../components/Layout';
+import Products from '../../components/Products';
+import Filters from '../../components/Filters';
+
+import styled from './catalog.module.scss';
+
+function Catalog() {
+    // const shortDescription = `${R.take(60, phone.description)}...`;
+
+    return (
+        <Layout>
+            <div className={styled.catalog}>
+                <div className={styled.products}>
+                    <Filters />
+                    <Products />
+                </div>
+            </div>
+        </Layout>
+    );
+}
+
+export default Catalog;
