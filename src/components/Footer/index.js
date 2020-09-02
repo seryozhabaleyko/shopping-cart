b94b09f2a93c2a51cebe9a4a8046de8d321c3ae3
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-02 22:10:00 +0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,10 +4,12 @@ import classNames from 'classnames';
 import styled from './footer.module.scss';
 
 function Footer() {
+    const classes = classNames('container', styled.topWrap);
+
     return (
         <footer className={styled.footer}>
             <section className={styled.top}>
-                <div className={classNames('container', styled.topWrap)}>
+                <div className={classes}>
                     <div>1</div>
                     <div className="social">
                         <div className="heading">Ищите нас в соц. сетях</div>
