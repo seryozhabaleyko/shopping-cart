9e117b12d5f7b79a4b91f23c91db4485c299b030
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-03 22:56:57 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
-import initialState from '../constants/initialState';
 
-const store = createStore(
-    rootReducer, initialState,
-    composeWithDevTools(
-        applyMiddleware(thunk),
-    ),
-);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
