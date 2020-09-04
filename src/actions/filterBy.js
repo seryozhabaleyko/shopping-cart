3f3786d30418e5859682d50dc2a36b2a771ec77c
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-04 23:47:53 +0300

    Update filterBy.js

--- src/actions/filterBy.js
+++ src/actions/filterBy.js
@@ -5,7 +5,37 @@ import {
     SET_MAX_PRICE_FILTER,
     ADD_RAM_TO_FILTER,
     REMOVE_RAM_FROM_FILTER,
+    ADD_FILTER_BY_BRAND,
+    ADD_FILTER_BY_PRICE,
+    REMOVE_FILTER_BY_BRAND,
+    REMOVE_FILTER_BY_PRICE,
+    SET_ORDER_BY,
 } from '../constants/actionTypes';
+
+export const addFilterByBrand = (brand) => ({
+    type: ADD_FILTER_BY_BRAND,
+    payload: brand,
+});
+
+export const removeFilterByBrand = (brand) => ({
+    type: REMOVE_FILTER_BY_BRAND,
+    payload: brand,
+});
+
+export const addFilterByPrice = (price) => ({
+    type: ADD_FILTER_BY_PRICE,
+    payload: price,
+});
+
+export const removeFilterByPrice = (price) => ({
+    type: REMOVE_FILTER_BY_PRICE,
+    payload: price,
+});
+
+export const setOrderBy = (orderBy) => ({
+    type: SET_ORDER_BY,
+    payload: orderBy,
+});
 
 export const addManufacturerToFilter = (manufacturer) => ({
     type: ADD_MANUFACTURER_TO_FILTER,
