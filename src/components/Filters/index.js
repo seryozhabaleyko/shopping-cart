3f37169995cac435ec5f0619af77cf5813dc1be0
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-04 23:48:09 +0300

    Update index.js

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Collapse } from 'antd';
 
 import Manufacturer from './Manufacturer';
 import RangeMinMaxPrice from './RangeMinMaxPrice';
@@ -7,12 +8,26 @@ import InternalStorage from './InternalStorage';
 
 import styled from './filters.module.scss';
 
+const { Panel } = Collapse;
+
 function Filters() {
     return (
         <div className={styled.filters}>
-            Filters
-            <Manufacturer />
-            <RangeMinMaxPrice />
+            <Collapse defaultActiveKey={['1', '2']} expandIconPosition="right">
+                <Panel header="Производитель" key="1">
+                    <Manufacturer />
+                </Panel>
+                <Panel header="Цены" key="2">
+                    <RangeMinMaxPrice />
+                </Panel>
+                <Panel header="Оперативная память" key="3">
+                    12
+                </Panel>
+                <Panel header="Флэш-память" key="4">
+                    12
+                </Panel>
+            </Collapse>
+
             <RAM />
             <InternalStorage />
         </div>
