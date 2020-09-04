1163d6d77f4cd51175a4a54f9e1690e5e0f6226d
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-04 23:49:58 +0300

    Update filterBy.js

--- src/reducers/filterBy.js
+++ src/reducers/filterBy.js
@@ -1,31 +1,35 @@
 import * as R from 'ramda';
 
 import {
-    ADD_MANUFACTURER_TO_FILTER,
-    REMOVE_MANUFACTURER_FROM_FILTER,
     SET_MIN_PRICE_FILTER,
     SET_MAX_PRICE_FILTER,
     ADD_RAM_TO_FILTER,
     REMOVE_RAM_FROM_FILTER,
+    ADD_FILTER_BY_BRAND,
+    ADD_FILTER_BY_PRICE,
+    REMOVE_FILTER_BY_BRAND,
+    REMOVE_FILTER_BY_PRICE,
+    SET_ORDER_BY,
 } from '../constants/actionTypes';
 
 const initialState = {
-    manufacturer: [],
+    brand: [],
     price: { min: null, max: null },
     ram: [],
+    orderBy: 'date-desc',
 };
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
-        case ADD_MANUFACTURER_TO_FILTER:
+        case ADD_FILTER_BY_BRAND:
             return {
                 ...state,
-                manufacturer: R.concat(R.prop('manufacturer', state), [payload]),
+                brand: R.concat(R.prop('brand', state), [payload]),
             };
-        case REMOVE_MANUFACTURER_FROM_FILTER:
+        case REMOVE_FILTER_BY_BRAND:
             return {
                 ...state,
-                manufacturer: R.without(payload, R.prop('manufacturer', state)),
+                brand: R.without(payload, R.prop('brand', state)),
             };
 
         case SET_MIN_PRICE_FILTER:
@@ -49,6 +53,9 @@ export default (state = initialState, { type, payload }) => {
                 ...state,
                 ram: R.without(payload, R.prop('ram', state)),
             };
+
+        case SET_ORDER_BY:
+            return { ...state, orderBy: payload };
 
         default:
             return state;
