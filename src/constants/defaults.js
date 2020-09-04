6d2488fe97384305552f247aa41c2eb275922f06
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-04 23:49:15 +0300

    Update defaults.js

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -1,13 +1,15 @@
 export const defaultErrorMessage = 'Сервер временно недоступен';
 export const order = { asc: 'ascending', desc: 'descending' };
 
-export const manufacturer = [
-    'Xiaomi',
-    'Apple',
-    'Samsung',
-    'Huawei',
-    'Google',
-    'Nokia',
-    'OnePlus',
-    'TeXet',
+export const manufacturer = ['Xiaomi', 'Apple', 'Samsung', 'Huawei', 'Google', 'Nokia', 'OnePlus', 'TeXet'];
+
+export const brand = [
+    { id: 1, label: 'Xiaomi', value: 'xiaomi' },
+    { id: 2, label: 'Apple', value: 'apple' },
+    { id: 3, label: 'Samsung', value: 'samsung' },
+    { id: 4, label: 'Huawei', value: 'huawei' },
+    { id: 5, label: 'Google', value: 'google' },
+    { id: 5, label: 'Nokia', value: 'nokia' },
+    { id: 6, label: 'OnePlus', value: 'onePlus' },
+    { id: 7, label: 'TeXet', value: 'teXet' },
 ];
