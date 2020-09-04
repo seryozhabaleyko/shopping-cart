7f9e93c3202ea73371ec4d2ef4d0a98e2a329cf1
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-04 23:49:55 +0300

    Update catalog.js

--- src/reducers/catalog.js
+++ src/reducers/catalog.js
@@ -7,38 +7,33 @@ import {
     LOAD_MORE_SUCCESS,
     LOAD_MORE_REQUEST,
     LOAD_MORE_FAILURE,
-    SORT_BY,
 } from '../constants/actionTypes';
 
 const initialState = {
-    isLoading: true,
-    data: {},
+    loading: true,
+    data: [],
     error: null,
-    sortBy: { key: 'price', type: 'ASC' },
 };
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
         case GET_CATALOG_REQUEST:
-            return { ...state, isLoading: true, error: null };
+            return { ...state, loading: true, error: null };
         case GET_CATALOG_SUCCESS:
-            return { ...state, isLoading: false, data: R.indexBy(R.prop('id'), payload) };
+            return { ...state, loading: false, data: payload };
         case GET_CATALOG_FAILURE:
-            return { ...state, isLoading: false, error: payload };
+            return { ...state, loading: false, error: payload };
 
         case LOAD_MORE_REQUEST:
-            return { ...state, isLoading: true, error: null };
+            return { ...state, loading: true, error: null };
         case LOAD_MORE_SUCCESS:
             return {
                 ...state,
-                isLoading: false,
+                loading: false,
                 data: R.merge(R.prop('data', state), R.indexBy(R.prop('id'), payload)),
             };
         case LOAD_MORE_FAILURE:
-            return { ...state, isLoading: false, error: payload };
-
-        case SORT_BY:
-            return { ...state, sortBy: payload };
+            return { ...state, loading: false, error: payload };
 
         default:
             return state;
