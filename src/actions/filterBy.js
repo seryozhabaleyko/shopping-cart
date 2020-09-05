6e3cf40b1aa87405cc5c37d88427383c61828253
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-05 16:50:33 +0300

    Update filterBy.js

--- src/actions/filterBy.js
+++ src/actions/filterBy.js
@@ -3,13 +3,15 @@ import {
     REMOVE_MANUFACTURER_FROM_FILTER,
     SET_MIN_PRICE_FILTER,
     SET_MAX_PRICE_FILTER,
-    ADD_RAM_TO_FILTER,
-    REMOVE_RAM_FROM_FILTER,
+    ADD_FILTER_BY_RAM,
+    REMOVE_FILTER_BY_RAM,
     ADD_FILTER_BY_BRAND,
     ADD_FILTER_BY_PRICE,
     REMOVE_FILTER_BY_BRAND,
     REMOVE_FILTER_BY_PRICE,
     SET_ORDER_BY,
+    ADD_FILTER_BY_INTERNAL_STORAGE,
+    REMOVE_FILTER_BY_INTERNAL_STORAGE,
 } from '../constants/actionTypes';
 
 export const addFilterByBrand = (brand) => ({
@@ -21,6 +23,30 @@ export const removeFilterByBrand = (brand) => ({
     type: REMOVE_FILTER_BY_BRAND,
     payload: brand,
 });
+
+export const addFilterByRAM = (ram) => ({
+    type: ADD_FILTER_BY_RAM,
+    payload: ram,
+});
+
+export const removeFilterByRAM = (ram) => ({
+    type: REMOVE_FILTER_BY_RAM,
+    payload: ram,
+});
+
+export const addFilterByInternalStorage = (gb) => ({
+    type: ADD_FILTER_BY_INTERNAL_STORAGE,
+    payload: gb,
+});
+
+export const removeFilterByInternalStorage = (gb) => ({
+    type: REMOVE_FILTER_BY_INTERNAL_STORAGE,
+    payload: gb,
+});
+
+/**
+ * olo
+ */
 
 export const addFilterByPrice = (price) => ({
     type: ADD_FILTER_BY_PRICE,
@@ -55,14 +81,4 @@ export const setMinPriceFilter = (minPrice) => ({
 export const setMaxPriceFilter = (maxPrice) => ({
     type: SET_MAX_PRICE_FILTER,
     payload: maxPrice,
-});
-
-export const addRAMToFilter = (RAM) => ({
-    type: ADD_RAM_TO_FILTER,
-    payload: RAM,
-});
-
-export const removeRAMFromFilter = (RAM) => ({
-    type: REMOVE_RAM_FROM_FILTER,
-    payload: RAM,
 });
