26ee9f2196791330afd26005fc84edc03621c232
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-05 16:50:44 +0300

    Update filterBy.js

--- src/reducers/filterBy.js
+++ src/reducers/filterBy.js
@@ -3,19 +3,22 @@ import * as R from 'ramda';
 import {
     SET_MIN_PRICE_FILTER,
     SET_MAX_PRICE_FILTER,
-    ADD_RAM_TO_FILTER,
-    REMOVE_RAM_FROM_FILTER,
+    ADD_FILTER_BY_RAM,
+    REMOVE_FILTER_BY_RAM,
     ADD_FILTER_BY_BRAND,
     ADD_FILTER_BY_PRICE,
     REMOVE_FILTER_BY_BRAND,
     REMOVE_FILTER_BY_PRICE,
     SET_ORDER_BY,
+    ADD_FILTER_BY_INTERNAL_STORAGE,
+    REMOVE_FILTER_BY_INTERNAL_STORAGE,
 } from '../constants/actionTypes';
 
 const initialState = {
     brand: [],
-    price: { min: null, max: null },
+    price: [0, 9999],
     ram: [],
+    internalStorage: [],
     orderBy: 'date-desc',
 };
 
@@ -24,12 +27,12 @@ export default (state = initialState, { type, payload }) => {
         case ADD_FILTER_BY_BRAND:
             return {
                 ...state,
-                brand: R.concat(R.prop('brand', state), [payload]),
+                brand: R.concat(R.prop('brand', state), [String(payload).toLowerCase()]),
             };
         case REMOVE_FILTER_BY_BRAND:
             return {
                 ...state,
-                brand: R.without(payload, R.prop('brand', state)),
+                brand: R.without(String(payload).toLowerCase(), R.prop('brand', state)),
             };
 
         case SET_MIN_PRICE_FILTER:
@@ -43,15 +46,26 @@ export default (state = initialState, { type, payload }) => {
                 price: R.assoc('max', payload, R.prop('price', state)),
             };
 
-        case ADD_RAM_TO_FILTER:
+        case ADD_FILTER_BY_RAM:
             return {
                 ...state,
-                ram: R.concat(R.prop('ram', state), [payload]),
+                ram: R.concat(R.prop('ram', state), [String(payload).toLowerCase()]),
             };
-        case REMOVE_RAM_FROM_FILTER:
+        case REMOVE_FILTER_BY_RAM:
             return {
                 ...state,
-                ram: R.without(payload, R.prop('ram', state)),
+                ram: R.without(String(payload).toLowerCase(), R.prop('ram', state)),
+            };
+
+        case ADD_FILTER_BY_INTERNAL_STORAGE:
+            return {
+                ...state,
+                internalStorage: R.concat(R.prop('internalStorage', state), [String(payload).toLowerCase()]),
+            };
+        case REMOVE_FILTER_BY_INTERNAL_STORAGE:
+            return {
+                ...state,
+                internalStorage: R.without(String(payload).toLowerCase(), R.prop('internalStorage', state)),
             };
 
         case SET_ORDER_BY:
