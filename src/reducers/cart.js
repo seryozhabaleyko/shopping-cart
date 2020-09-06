7f9dcfbb027d048fad16efe0a2d2a70eb2989f9f
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-07 02:29:38 +0300

    Update cart.js

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,17 +1,35 @@
-import * as R from 'ramda';
-
-import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from '../constants/actionTypes';
+import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART, ADD_QTY_ITEM, MINUS_QTY_ITEM } from '../constants/actionTypes';
 
 const initialState = [];
 
 export default (state = initialState, { type, payload }) => {
     switch (type) {
         case ADD_TO_CART:
-            return R.append(payload, state);
+            return state.some((product) => product.id === payload.id) ? state : [...state, { ...payload, quantity: 1 }];
         case REMOVE_FROM_CART:
-            return R.without(R.of(payload), state);
+            return state.filter((product) => product.id !== payload);
         case CLEAR_CART:
             return initialState;
+        case ADD_QTY_ITEM:
+            return state.map((product) => {
+                if (product.id === payload) {
+                    return {
+                        ...product,
+                        quantity: product.quantity + 1,
+                    };
+                }
+                return product;
+            });
+        case MINUS_QTY_ITEM:
+            return state.map((product) => {
+                if (product.id === payload) {
+                    return {
+                        ...product,
+                        quantity: product.quantity - 1,
+                    };
+                }
+                return product;
+            });
         default:
             return state;
     }
