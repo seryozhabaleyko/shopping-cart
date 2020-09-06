a6f6cd363c8c9b9f45cd836d26b7da412a5e0856
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-07 02:28:54 +0300

    Update index.js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Badge, Tooltip } from 'antd';
-import { ShoppingCartOutlined } from '@ant-design/icons';
+import { ShoppingCartOutlined, ShoppingFilled, ShoppingOutlined } from '@ant-design/icons';
 
 import { getNumberOfProducts, getTheTotalCostOfTheBasket } from '../../selectors';
 import useShallowEqualSelector from '../../hooks/useShallowEqualSelector';
@@ -13,10 +13,10 @@ function Cart() {
     const totalCartValue = useShallowEqualSelector(getTheTotalCostOfTheBasket);
 
     return (
-        <Tooltip title={`$${totalCartValue}`}>
+        <Tooltip title={`$${totalCartValue}`} placement="bottom">
             <Link to="/cart" className={styled.cart}>
-                <Badge count={numberProductsInCart} size="small" showZero>
-                    <ShoppingCartOutlined style={{ fontSize: '24px', color: '#9e9e9e' }} />
+                <Badge count={numberProductsInCart}>
+                    <ShoppingOutlined style={{ fontSize: '24px', color: '#555' }} />
                 </Badge>
             </Link>
         </Tooltip>
