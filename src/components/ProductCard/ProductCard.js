d10b1e6e1e500e9387d6e78280ff6afd1447d6c4
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-06 16:24:25 +0300

    Update ProductCard.js

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,43 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import classNames from 'classnames';
-import { Typography, Image, Badge, Card, Rate } from 'antd';
+import { Typography, Badge, Rate, Button } from 'antd';
+import { ShoppingCartOutlined } from '@ant-design/icons';
 
 import { addToCart } from '../../actions/cart';
 
-import styled from './product.module.scss';
 import './ProductCard.scss';
 
 const { Title } = Typography;
 
-function ProductCard({ id, title, photoUrl, price = 0, rating = 4 }) {
+function ProductCard({ id, title, photoUrl, price = 0, rating = 0 }) {
     const dispatch = useDispatch();
 
     return (
         <Badge.Ribbon text="В наличии" placement="start">
             <div className="product-card">
-                <div className={styled.image}>
+                <div className="product-card__image">
                     <Link to={`/product/${id}`}>
-                        <Image src={photoUrl} loading="lazy" alt={title} style={{ display: 'block' }} />
+                        <img src={photoUrl} loading="lazy" alt={title} style={{ display: 'block' }} />
                     </Link>
                 </div>
 
-                <div className={styled.detail}>
-                    <Title level={5} className={styled.title}>
-                        {title}
-                    </Title>
-                    <div className={styled.price}>{`$${price.toLocaleString()}`}</div>
+                <div className="product-card__content">
+                    <Link to={`/product/${id}`}>
+                        <Title className="product-card__title" level={5}>
+                            {title}
+                        </Title>
+                    </Link>
+
+                    <div className="product-card__price">{`$${price.toLocaleString()}`}</div>
 
                     <Rate disabled defaultValue={rating} />
 
-                    <button
-                        type="button"
-                        className={classNames('btn', 'btn-danger', styled.addToCart)}
+                    <Button
+                        type="primary"
+                        size="large"
+                        icon={<ShoppingCartOutlined />}
+                        style={{ marginTop: '1rem' }}
+                        block
                         onClick={() => dispatch(addToCart(id))}
                     >
-                        Add to Cart
-                    </button>
+                        В корзину
+                    </Button>
                 </div>
             </div>
         </Badge.Ribbon>
