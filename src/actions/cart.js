a63afc14b43b2dce91deef6c8bf485596e2b1538
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-07 02:28:52 +0300

    Update cart.js

--- src/actions/cart.js
+++ src/actions/cart.js
@@ -1,25 +1,29 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from '../constants/actionTypes';
+import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART, ADD_QTY_ITEM, MINUS_QTY_ITEM } from '../constants/actionTypes';
 
-export const addToCart = (id) => (dispatch) => {
-    dispatch({
-        type: ADD_TO_CART,
-        payload: id,
-    });
-};
+export const addToBasket = (product) => ({
+    type: ADD_TO_CART,
+    payload: product,
+});
 
-export const removePhoneFromBasket = (id) => (dispatch) => {
-    dispatch({
-        type: REMOVE_FROM_CART,
-        payload: id,
-    });
-};
+export const removeFromBasket = (id) => ({
+    type: REMOVE_FROM_CART,
+    payload: id,
+});
 
-export const cleanBasket = () => (dispatch) => {
-    dispatch({
-        type: CLEAR_CART,
-    });
-};
+export const cleanBasket = () => ({
+    type: CLEAR_CART,
+});
 
 export const basketCheckout = (phones) => () => {
     alert(JSON.stringify(phones));
 };
+
+export const addQtyItem = (id) => ({
+    type: ADD_QTY_ITEM,
+    payload: id,
+});
+
+export const minusQtyItem = (id) => ({
+    type: MINUS_QTY_ITEM,
+    payload: id,
+});
