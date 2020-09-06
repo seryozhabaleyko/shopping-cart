f53d90efd77dcb288a9f973d4baaea6a6e9e641e
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-07 02:29:11 +0300

    Update CatalogContainer.js

--- src/containers/CatalogContainer.js
+++ src/containers/CatalogContainer.js
@@ -4,8 +4,10 @@ import { useDispatch } from 'react-redux';
 import { fetchCatalog } from '../actions/catalog';
 import { fetchCategories } from '../actions/categories';
 import Catalog from '../pages/Catalog';
+import useDocumentTitle from '../hooks/useDocumentTitle';
 
 function CatalogContainer() {
+    useDocumentTitle('Каталог товаров | Valhalla - Online Store');
     const dispatch = useDispatch();
 
     useEffect(() => {
