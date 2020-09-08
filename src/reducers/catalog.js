9f83962cffa78ca6e91f2b2b4552dadd4f188b85
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-08 16:40:55 +0300

    Update catalog.js

--- src/reducers/catalog.js
+++ src/reducers/catalog.js
@@ -1,18 +1,19 @@
-import * as R from 'ramda';
-
 import {
     GET_CATALOG_REQUEST,
     GET_CATALOG_SUCCESS,
     GET_CATALOG_FAILURE,
-    LOAD_MORE_SUCCESS,
-    LOAD_MORE_REQUEST,
-    LOAD_MORE_FAILURE,
+    LOAD_MORE_PRODUCTS_REQUEST,
+    LOAD_MORE_PRODUCTS_SUCCESS,
+    LOAD_MORE_PRODUCTS_FAILURE,
 } from '../constants/actionTypes';
 
 const initialState = {
     loading: true,
     data: [],
+    lastVisible: null,
+    total: 0,
     error: null,
+    loadingLoadMore: false,
 };
 
 export default (state = initialState, { type, payload }) => {
@@ -20,20 +21,27 @@ export default (state = initialState, { type, payload }) => {
         case GET_CATALOG_REQUEST:
             return { ...state, loading: true, error: null };
         case GET_CATALOG_SUCCESS:
-            return { ...state, loading: false, data: payload };
-        case GET_CATALOG_FAILURE:
-            return { ...state, loading: false, error: payload };
-
-        case LOAD_MORE_REQUEST:
-            return { ...state, loading: true, error: null };
-        case LOAD_MORE_SUCCESS:
             return {
                 ...state,
                 loading: false,
-                data: R.merge(R.prop('data', state), R.indexBy(R.prop('id'), payload)),
+                data: payload.products,
+                lastVisible: payload.lastVisible,
+                total: payload.total,
             };
-        case LOAD_MORE_FAILURE:
+        case GET_CATALOG_FAILURE:
             return { ...state, loading: false, error: payload };
+
+        case LOAD_MORE_PRODUCTS_REQUEST:
+            return { ...state, loadingLoadMore: true, error: null };
+        case LOAD_MORE_PRODUCTS_SUCCESS:
+            return {
+                ...state,
+                loadingLoadMore: false,
+                data: [...state.data, ...payload.products],
+                lastVisible: payload.lastVisible,
+            };
+        case LOAD_MORE_PRODUCTS_FAILURE:
+            return { ...state, loadingLoadMore: false, error: payload };
 
         default:
             return state;
