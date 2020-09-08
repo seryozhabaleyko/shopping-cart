be448397f0a9f97641e5b8b9bc727ea42e1e784a
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-08 16:39:59 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -9,11 +9,11 @@ export const fetchCatalog = async () =>
         setTimeout(() => resolve(catalog), 1000);
     });
 
-export const fetchProductByIdApi = async (id) =>
+/* export const fetchProductByIdApi = async (id) =>
     new Promise((resolve, reject) => {
         const product = R.find(R.propEq('id', id), catalog);
         resolve(product);
-    });
+    }); */
 
 export const loadMorePhones = async ({ offset }) =>
     new Promise((resolve, reject) => {
@@ -31,8 +31,33 @@ export const fetchCategories = async () =>
         resolve(categories);
     });
 
-export function fetchCatalogApi({ limit = 8 }) {
+export async function fetchCatalogApi({ limit = 8 }) {
+    const totalQuery = await db.collection('catalog').get();
+    const total = totalQuery.docs.length;
+
     const catalogCollectionRef = db.collection('catalog').orderBy('createdAt', 'desc').limit(limit);
 
-    return catalogCollectionRef.get().then((snapshot) => snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    return catalogCollectionRef.get().then((snapshot) => {
+        const lastVisible = snapshot.docs[snapshot.docs.length - 1];
+        const products = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+        return { products, lastVisible, total };
+    });
+}
+
+export function loadMoreProductsApi(lastVisible) {
+    const productsRef = db.collection('catalog').orderBy('createdAt', 'desc').startAfter(lastVisible).limit(25);
+
+    return productsRef.get().then((snapshot) => {
+        const lastVisible = snapshot.docs[snapshot.docs.length - 1];
+        const products = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+
+        return { products, lastVisible };
+    });
+}
+
+export function fetchProductByIdApi(productId) {
+    const productRef = db.collection('catalog').doc(productId);
+
+    return productRef.get().then((doc) => ({ ...doc.data(), id: doc.id }));
 }
