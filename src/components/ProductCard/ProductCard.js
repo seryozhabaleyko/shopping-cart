255414a3c19554610ec51c66143a9e21246c95ec
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-10 22:25:13 +0300

    Update ProductCard.js

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -13,60 +13,64 @@ const { Title } = Typography;
 
 function ProductCard({ product, foundOnBasket }) {
     const dispatch = useDispatch();
-    const { id, title, photoUrl, price = 0, rating = 0 } = product;
+    const { id, title, photoUrl, price = 0, rating = 0, maxQuantity = 0 } = product;
 
     const handleAddToBasketClick = () => dispatch(addToBasket(product));
     const handleRemoveFromBasketClick = () => dispatch(removeFromBasket(product.id));
 
+    const ribbonProps = maxQuantity ? { text: 'В наличии' } : { text: 'Нет в наличии', color: 'cyan' };
+
     return (
-        <Badge.Ribbon text="В наличии" placement="start">
-            <div className="product-card">
+        <div className="product-card">
+            <Badge.Ribbon {...ribbonProps} placement="start">
                 <div className="product-card__image">
                     <Link to={`/product/${id}`}>
                         <img src={photoUrl} loading="lazy" alt={title} style={{ display: 'block' }} />
                     </Link>
                 </div>
+            </Badge.Ribbon>
 
-                <div className="product-card__content">
-                    <Link to={`/product/${id}`}>
-                        <Title className="product-card__title" level={5}>
-                            {title}
-                        </Title>
-                    </Link>
+            <div className="product-card__content">
+                <Link to={`/product/${id}`}>
+                    <Title className="product-card__title" level={5}>
+                        {title}
+                    </Title>
+                </Link>
 
-                    <div className="product-card__price">{`$${price.toLocaleString()}`}</div>
+                <div className="product-card__price">{`$${price.toLocaleString()}`}</div>
 
-                    <Rate disabled defaultValue={rating} />
+                <Rate disabled defaultValue={rating} allowHalf />
 
-                    {foundOnBasket ? (
-                        <Button
-                            type="primary"
-                            size="large"
-                            icon={<DeleteOutlined />}
-                            style={{ marginTop: '1rem' }}
-                            block
-                            danger
-                            onClick={handleRemoveFromBasketClick}
-                        >
-                            Из корзины
-                        </Button>
-                    ) : (
-                        <Button
-                            type="primary"
-                            size="large"
-                            icon={<ShoppingCartOutlined />}
-                            style={{ marginTop: '1rem' }}
-                            block
-                            onClick={handleAddToBasketClick}
-                        >
-                            В корзину
-                        </Button>
-                    )}
-                </div>
+                {foundOnBasket ? (
+                    <Button
+                        type="primary"
+                        size="large"
+                        icon={<DeleteOutlined />}
+                        style={{ marginTop: '1rem' }}
+                        block
+                        danger
+                        onClick={handleRemoveFromBasketClick}
+                    >
+                        Из корзины
+                    </Button>
+                ) : (
+                    <Button
+                        type="primary"
+                        size="large"
+                        icon={<ShoppingCartOutlined />}
+                        style={{ marginTop: '1rem' }}
+                        block
+                        onClick={handleAddToBasketClick}
+                        disabled={!maxQuantity}
+                    >
+                        В корзину
+                    </Button>
+                )}
             </div>
-        </Badge.Ribbon>
+        </div>
     );
 }
+
 ProductCard.propType = {
     product: PropTypes.object.isRequired,
     foundOnBasket: PropTypes.bool,
