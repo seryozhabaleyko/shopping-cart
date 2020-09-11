21bc3d35efe39ea76a5b204200c2f90ef4708fdf
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-11 22:22:31 +0300

    Update selectors.js

--- src/selectors.js
+++ src/selectors.js
@@ -1,20 +1,21 @@
 import * as R from 'ramda';
 import { createSelector } from 'reselect';
 
-const cartSelector = (state) => state.cart;
-const numberItemsCartSelector = (state) => state.cart.length;
+export const getCart = createSelector(
+    (state) => state.cart,
+    (cart) => cart,
+);
 
-export const getCart = createSelector(cartSelector, (cart) => cart);
-export const getNumberItemsCart = createSelector(numberItemsCartSelector, (numberItemsCart) => numberItemsCart);
-export const getTotalPrice = createSelector(cartSelector, (cart) => {
+export const getNumberCartItems = createSelector(getCart, (cart) => cart.length);
+
+export const getTotalPrice = createSelector(getCart, (cart) => {
     let total = 0;
 
     if (cart.length !== 0) {
-        const result = cart.map((product) => product.price * product.quantity).reduce((a, b) => a + b);
-        total = result.toFixed(2);
+        total = cart.map((product) => product.price * product.quantity).reduce((a, b) => a + b);
     }
 
-    return Number(total).toLocaleString();
+    return total;
 });
 
 const productLoading = (state) => state.product.isLoading;
@@ -94,54 +95,3 @@ export const getCatalog = createSelector(
         };
     },
 );
-
-const getActionCategoryId = (props) => R.path(['match', 'params', 'id'], props);
-export const getPhoneById = (state, id) => R.prop(id, state.catalog.data);
-
-export function getPhones(state, props) {
-    const activeCategoryId = getActionCategoryId(props);
-
-    const applySearch = (item) => R.contains(state.phonesPage.search, R.prop('name', item));
-
-    const applyCategory = (item) => R.equals(activeCategoryId, R.prop('categoryId', item));
-
-    const phones = R.compose(
-        R.filter(applySearch),
-        R.when(R.always(activeCategoryId), R.filter(applyCategory)),
-        R.map((id) => getPhoneById(state, id)),
-    )(state.phonesPage.ids);
-
-    return phones;
-}
-
-export const getRenderedPhonesLength = (state) => R.length(state.phonesPage.ids);
-
-export const getNumberOfProducts = (state) => R.length(state.cart);
-
-export const getTheTotalCostOfTheBasket = (state) => {
-    const totalPrice = R.compose(
-        R.sum,
-        R.pluck('price'),
-        R.map((id) => getPhoneById(state, id)),
-    )(state.cart);
-
-    return totalPrice.toLocaleString();
-};
-
-export const getCategories = (state) => R.values(state.categories);
-
-export const getBasketPhonesWithCount = (state) => {
-    const phoneCount = (id) =>
-        R.compose(
-            R.length,
-            R.filter((basketId) => R.equals(id, basketId)),
-        )(state.cart);
-    const phoneWithCount = (phone) => R.assoc('count', phoneCount(phone.id), phone);
-    const uniqueIds = R.uniq(state.cart);
-    const phones = R.compose(
-        R.map(phoneWithCount),
-        R.map((id) => getPhoneById(state, id)),
-    )(uniqueIds);
-
-    return phones;
-};
