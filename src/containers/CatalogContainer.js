6f410332c2bb43e8c9f7d904e3ef994bf915b91a
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-11 22:22:05 +0300

    Update CatalogContainer.js

--- src/containers/CatalogContainer.js
+++ src/containers/CatalogContainer.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useDispatch, shallowEqual, useSelector } from 'react-redux';
 
 import { fetchCatalog } from '../actions/catalog';
-import { fetchCategories } from '../actions/categories';
 import Catalog from '../pages/Catalog';
 import useDocumentTitle from '../hooks/useDocumentTitle';
 
@@ -15,7 +14,6 @@ function CatalogContainer() {
         if (productsLength === 0) {
             dispatch(fetchCatalog({ limit: 8 }));
         }
-        dispatch(fetchCategories());
     }, [dispatch, productsLength]);
 
     return <Catalog />;
