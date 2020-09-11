2ba0c6c086d95c2164a5d3ecc8bae54903a741bc
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-11 22:21:41 +0300

    Create Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from '../Navbar';
+
+import './Header.scss';
+
+function Header() {
+    return (
+        <header className="header">
+            <Navbar />
+        </header>
+    );
+}
+
+export default Header;
