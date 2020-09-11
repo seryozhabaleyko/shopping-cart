0440860ce2f0984b9e8d47acbf6b55739a4e3bc3
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-11 22:20:50 +0300

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,35 +1,4 @@
 import { db } from '../services/firebase';
-import * as R from 'ramda';
-
-import catalog from './mockCatalog';
-import categories from './mockCategories';
-
-export const fetchCatalog = async () =>
-    new Promise((resolve, reject) => {
-        setTimeout(() => resolve(catalog), 1000);
-    });
-
-/* export const fetchProductByIdApi = async (id) =>
-    new Promise((resolve, reject) => {
-        const product = R.find(R.propEq('id', id), catalog);
-        resolve(product);
-    }); */
-
-export const loadMorePhones = async ({ offset }) =>
-    new Promise((resolve, reject) => {
-        resolve(catalog);
-    });
-
-export const fetchPhoneById = async (id) =>
-    new Promise((resolve, reject) => {
-        const phone = R.find(R.propEq('id', id), catalog);
-        resolve(phone);
-    });
-
-export const fetchCategories = async () =>
-    new Promise((resolve, reject) => {
-        resolve(categories);
-    });
 
 export async function fetchCatalogApi({ limit = 8 }) {
     const totalQuery = await db.collection('catalog').get();
