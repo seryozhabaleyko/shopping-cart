ba1948d17c2abee3fb96fb57c97af2e2b22e9f4d
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-11 22:21:49 +0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,1 @@
-import React from 'react';
-
-import Navbar from '../Navbar';
-
-import styled from './header.module.scss';
-
-function Header() {
-	return (
-		<header className={styled.header}>
-			<Navbar />
-		</header>
-	);
-}
-
-export default Header;
+export { default } from './Header';
