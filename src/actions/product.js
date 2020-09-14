062235877481fc31c18d79c742696f55abe20ff4
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-14 12:26:33 +0300

    Update product.js

--- src/actions/product.js
+++ src/actions/product.js
@@ -1,31 +1,18 @@
-import {
-    GET_PRODUCT_REQUEST,
-    GET_PRODUCT_SUCCESS,
-    GET_PRODUCT_FAILURE,
-} from '../constants/actionTypes';
-import { defaultErrorMessage } from '../constants/defaults';
-import { fetchProductByIdApi } from '../api';
+import { GET_PRODUCT_REQUEST, GET_PRODUCT_SUCCESS, GET_PRODUCT_FAILURE } from '../constants/actionTypes';
 
-const productRequest = () => ({
-    type: GET_PRODUCT_REQUEST,
-});
-
-const productSuccess = (product) => ({
+export const fetchProductSuccess = (product) => ({
     type: GET_PRODUCT_SUCCESS,
     payload: product,
 });
 
-const productFailure = (errorMessage = defaultErrorMessage, errorName) => ({
+export const fetchProductFailure = (error) => ({
     type: GET_PRODUCT_FAILURE,
-    payload: { message: errorMessage, name: errorName },
+    payload: error,
 });
 
 export const fetchProductById = (id) => async (dispatch) => {
-    dispatch(productRequest());
-    try {
-        const product = await fetchProductByIdApi(id);
-        dispatch(productSuccess(product));
-    } catch (error) {
-        dispatch(productFailure(error.message, error.name));
-    }
+    dispatch({
+        type: GET_PRODUCT_REQUEST,
+        payload: { id },
+    });
 };
