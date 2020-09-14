0ca8536423fe5cecdb8077492c9fe0585ada6e56
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-14 12:26:31 +0300

    Update catalog.js

--- src/actions/catalog.js
+++ src/actions/catalog.js
@@ -1,4 +1,3 @@
-import { fetchCatalogApi, loadMoreProductsApi } from '../api';
 import {
     GET_CATALOG_REQUEST,
     GET_CATALOG_SUCCESS,
@@ -7,52 +6,37 @@ import {
     LOAD_MORE_PRODUCTS_SUCCESS,
     LOAD_MORE_PRODUCTS_FAILURE,
 } from '../constants/actionTypes';
-import { defaultErrorMessage } from '../constants/defaults';
 
-const fetchCatalogRequest = () => ({
-    type: GET_CATALOG_REQUEST,
-});
-
-const fetchCatalogSuccess = (phones) => ({
+export const fetchProductsSuccess = (products) => ({
     type: GET_CATALOG_SUCCESS,
-    payload: phones,
+    payload: products,
 });
 
-const fetchCatalogFailure = (errorMessage = defaultErrorMessage, errorName) => ({
+export const fetchProductsFailure = (error) => ({
     type: GET_CATALOG_FAILURE,
-    payload: { message: errorMessage, name: errorName },
+    payload: error,
 });
 
-export const fetchCatalog = (queryParameters) => async (dispatch) => {
-    dispatch(fetchCatalogRequest());
-    try {
-        const catalog = await fetchCatalogApi(queryParameters);
-        dispatch(fetchCatalogSuccess(catalog));
-    } catch (error) {
-        dispatch(fetchCatalogFailure(error.message, error.name));
-    }
+export const fetchProducts = (data) => (dispatch) => {
+    dispatch({
+        type: GET_CATALOG_REQUEST,
+        payload: data,
+    });
 };
 
-const loadMoreProductsRequest = () => ({
-    type: LOAD_MORE_PRODUCTS_REQUEST,
-});
-
-const loadMoreProductsSuccess = (data) => ({
+export const loadMoreProductsSuccess = (data) => ({
     type: LOAD_MORE_PRODUCTS_SUCCESS,
     payload: data,
 });
 
-const loadMoreProductsFailure = (error) => ({
+export const loadMoreProductsFailure = (error) => ({
     type: LOAD_MORE_PRODUCTS_FAILURE,
     payload: error,
 });
 
 export const loadMoreProducts = (lastVisible) => async (dispatch) => {
-    dispatch(loadMoreProductsRequest());
-    try {
-        const data = await loadMoreProductsApi(lastVisible);
-        dispatch(loadMoreProductsSuccess(data));
-    } catch (error) {
-        dispatch(loadMoreProductsFailure(error));
-    }
+    dispatch({
+        type: LOAD_MORE_PRODUCTS_REQUEST,
+        payload: { lastVisible },
+    });
 };
