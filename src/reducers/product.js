d9e68e276a0575d9258f276d3e25c37f3f161c4d
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-14 12:26:52 +0300

    Update product.js

--- src/reducers/product.js
+++ src/reducers/product.js
@@ -14,6 +14,7 @@ export default (state = initialState, { type, payload }) => {
             return { ...state, loading: false, data: payload };
         case GET_PRODUCT_FAILURE:
             return { ...state, loading: false, error: payload };
+
         default:
             return state;
     }
