b82d357a8dc2e0d5ee851262873b7d40d9600497
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-14 12:27:03 +0300

    Delete index.js

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-
-import rootReducer from '../reducers';
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
-export default store;
