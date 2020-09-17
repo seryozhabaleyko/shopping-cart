acd71b9cf19c0ecd3c515c7995a28e541083c22a
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-17 18:30:53 +0300

    Create configureStore.ts

---
+++ src/store/configureStore.ts
@@ -0,0 +1,23 @@
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from '../reducers';
+
+function configureStore(initialState: any) {
+    const sagaMiddleware = createSagaMiddleware();
+
+    return {
+        ...createStore(
+            rootReducer,
+            initialState,
+            compose(
+                applyMiddleware(thunkMiddleware, sagaMiddleware),
+                (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+            ),
+        ),
+        runSaga: sagaMiddleware.run,
+    };
+}
+
+export default configureStore;
