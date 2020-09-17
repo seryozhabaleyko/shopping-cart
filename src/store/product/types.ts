7950d668c73566ea08eec1d8fcbcb37363d84342
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-17 18:31:14 +0300

    Create types.ts

---
+++ src/store/product/types.ts
@@ -0,0 +1,47 @@
+export type Loading = boolean;
+export type Error = object | string | null;
+export interface Product {
+    id: string;
+    title: string;
+    photoUrl: string;
+    price: number;
+    rating: number;
+    quantity: number;
+    maxQuantity: number;
+}
+
+export interface ProductState {
+    readonly loading: Loading;
+    readonly data: Product;
+    readonly error: Error;
+}
+
+export const FETCH_PRODUCT = 'FETCH_PRODUCT';
+
+export interface FetchProduct {
+    type: typeof FETCH_PRODUCT;
+    id: string;
+}
+
+export const FETCH_PRODUCT_REQUEST = 'FETCH_PRODUCT_REQUEST';
+
+export interface FetchProductRequest {
+    type: typeof FETCH_PRODUCT_REQUEST;
+    payload?: any;
+}
+
+export const FETCH_PRODUCT_SUCCESS = 'FETCH_PRODUCT_SUCCESS';
+
+export interface FetchProductSuccess {
+    type: typeof FETCH_PRODUCT_SUCCESS;
+    payload: Product;
+}
+
+export const FETCH_PRODUCT_FAILURE = 'FETCH_PRODUCT_FAILURE';
+
+export interface FetchProductFailure {
+    type: typeof FETCH_PRODUCT_FAILURE;
+    payload: Error;
+}
+
+export type ProductActionsTypes = FetchProductRequest | FetchProductSuccess | FetchProductFailure;
