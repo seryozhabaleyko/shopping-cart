2add59c77a06befbe8f332da582d61d67cef7115
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-17 18:31:05 +0300

    Create reducers.ts

---
+++ src/store/product/reducers.ts
@@ -0,0 +1,42 @@
+import { Reducer } from 'redux';
+
+import {
+    ProductState,
+    ProductActionsTypes,
+    FETCH_PRODUCT_REQUEST,
+    FETCH_PRODUCT_SUCCESS,
+    FETCH_PRODUCT_FAILURE,
+} from './types';
+
+const initialState: ProductState = {
+    loading: false,
+    data: {
+        id: '',
+        title: '',
+        photoUrl: '',
+        price: 0,
+        rating: 0,
+        quantity: 0,
+        maxQuantity: 0,
+    },
+    error: null,
+};
+
+const productReducer: Reducer<ProductState, ProductActionsTypes> = (
+    state: ProductState = initialState,
+    { type, payload }: ProductActionsTypes,
+): ProductState => {
+    switch (type) {
+        case FETCH_PRODUCT_REQUEST:
+            return { ...state, loading: true, error: null };
+        case FETCH_PRODUCT_SUCCESS:
+            return { ...state, loading: false, data: payload };
+        case FETCH_PRODUCT_FAILURE:
+            return { ...state, loading: false, error: payload };
+
+        default:
+            return state;
+    }
+};
+
+export default productReducer;
