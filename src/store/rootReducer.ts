317aaa03ef1055e56cc4a739125150180b0fa27d
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-18 18:58:56 +0300

    Update rootReducer.ts

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,9 +1,15 @@
 import { combineReducers } from 'redux';
 
 import productReducer from './product/reducers';
+import productsReducer from './products/reducers';
+import cartReducer from './cart/reducers';
+import filterByReducer from './filterBy/reducers';
 
 const rootReducer = combineReducers({
     product: productReducer,
+    catalog: productsReducer,
+    cart: cartReducer,
+    filterBy: filterByReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
