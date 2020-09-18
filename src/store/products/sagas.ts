889b8e5c2bd0f8cbe8ea61669a23e92704ab9913
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-18 18:58:44 +0300

    Update sagas.ts

--- src/store/products/sagas.ts
+++ src/store/products/sagas.ts
@@ -1,7 +1,14 @@
 import { put, call, takeEvery, all, fork } from 'redux-saga/effects';
-import { FETCH_PRODUCTS } from './../products/types';
-import { fetchProductsRequest, fetchProductsSuccess, fetchProductsFailure } from './actions';
-import { fetchProductsApi } from '../../api';
+import { FETCH_PRODUCTS, LOAD_MORE_PRODUCTS, LoadMoreProducts } from './../products/types';
+import {
+    fetchProductsRequest,
+    fetchProductsSuccess,
+    fetchProductsFailure,
+    loadMoreProductsSuccess,
+    loadMoreProductsFailure,
+    loadMoreProductsRequest,
+} from './actions';
+import { fetchProductsApi, loadMoreProductsApi } from '../../api';
 
 function* fetchProducts() {
     try {
@@ -13,8 +20,19 @@ function* fetchProducts() {
     }
 }
 
+function* loadMoreProducts({ lastVisible }: LoadMoreProducts) {
+    try {
+        yield put(loadMoreProductsRequest());
+        const products = yield call(loadMoreProductsApi, lastVisible);
+        yield put(loadMoreProductsSuccess(products));
+    } catch (error) {
+        yield put(loadMoreProductsFailure(error));
+    }
+}
+
 function* watchFetchProducts() {
     yield takeEvery(FETCH_PRODUCTS, fetchProducts);
+    yield takeEvery(LOAD_MORE_PRODUCTS, loadMoreProducts);
 }
 
 function* productsSaga() {
