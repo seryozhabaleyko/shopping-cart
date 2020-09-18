e2cb3834e8d21337fa6271881f4b0bbd609538fb
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-18 18:59:45 +0300

    Delete useDocumentTitle.js

--- src/hooks/useDocumentTitle.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-function useDocumentTitle(title) {
-    useEffect(() => {
-        document.title = String(title);
-    }, [title]);
-}
-
-export default useDocumentTitle;
