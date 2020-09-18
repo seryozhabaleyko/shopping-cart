17295d8267428a2b244b926be442bf75d7af1c73
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-18 18:59:58 +0300

    Delete useShallowEqualSelector.js

--- src/hooks/useShallowEqualSelector.js
+++
@@ -1,7 +0,0 @@
-import { useSelector, shallowEqual } from 'react-redux';
-
-export function useShallowEqualSelector(selector) {
-    return useSelector(selector, shallowEqual);
-}
-
-export default useShallowEqualSelector;
