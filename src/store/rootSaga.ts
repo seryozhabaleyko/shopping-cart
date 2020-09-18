e288d23d9b1bbe70b6b8454947a497a589d2d401
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-18 18:59:03 +0300

    Update rootSaga.ts

--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,8 +1,9 @@
 import { all, fork } from 'redux-saga/effects';
 import productSaga from './product/sagas';
+import productsSaga from './products/sagas';
 
 function* rootSaga() {
-    yield all([fork(productSaga)]);
+    yield all([fork(productSaga), fork(productsSaga)]);
 }
 
 export default rootSaga;
